b060532bb5b57746658287ba4d39c7df827430d9
JudasPringles <kevinoftroy1@gmail.com>
2026-09-23 00:14:11 +0100

    Make Sessions folder public

--- plugins/exclude-unpublished/dist/index.js
+++ plugins/exclude-unpublished/dist/index.js
@@ -2,8 +2,6 @@ function ExcludeUnpublished() {
   return {
     name: "ExcludeUnpublished",
     shouldPublish(_ctx, [_tree, vfile]) {
-      const slug = vfile.data?.slug ?? ""
-      if (slug.startsWith("Sessions/") || slug.startsWith("sessions/")) return false
       const publish = vfile.data?.frontmatter?.publish
       if (publish === false || publish === "false") return false
       return true
